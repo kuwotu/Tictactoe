132d295b216a49c6e2ddf988c0c6d8f9e101c784
Kevwe Uwotu <kevwe1996@gmail.com>
2021-08-02 23:26:42 +0100

    render testing

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
 (function () {
   const Gameboard = {
-    gameboard: [],
+    gameboard: ["O", "O", "O"],
+
     addMark: function () {
       const boardSquare = document.getElementsByClassName("board-square");
       for (i = 0; i < boardSquare.length; i++) {
-        boardSquare[i].addEventListener("click", (event) => {
-          event.target.getAttribute("id");
-        });
+        xMarksTheSpot = (event) => {
+          const spot = event.target;
+          spot.textContent = "X";
+        };
+        boardSquare[i].addEventListener("click", xMarksTheSpot);
+      }
+    },
+    render: function () {
+      const boardSquare = document.getElementsByClassName("board-square");
+      const score = this.gameboard;
+      for (i = 0; i < score.length; i++) {
+        console.log((boardSquare[i].textContent = score[i]));
       }
     },
   };
+
+  const Player = () => {
+    const playerSpots = [];
+  };
+
   Gameboard.addMark();
+  Gameboard.render();
 })();
