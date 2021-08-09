f7cb6fb1851696c5349afafe9a94b96df8ec8d16
Kevwe Uwotu <kevwe1996@gmail.com>
2021-08-09 23:41:39 +0100

    render to spot added

--- script.js
+++ script.js
@@ -1,30 +1,40 @@
 (function () {
   const Gameboard = {
-    gameboard: ["O", "O", "O"],
+    gameboard: [],
 
-    addMark: function () {
+    addMarker: function () {
       const boardSquare = document.getElementsByClassName("board-square");
       for (i = 0; i < boardSquare.length; i++) {
-        xMarksTheSpot = (event) => {
+        markTheSpot = (event) => {
           const spot = event.target;
-          spot.textContent = "X";
+          console.log(this.gameboard);
+          player1();
+          Gameboard.render(spot);
         };
-        boardSquare[i].addEventListener("click", xMarksTheSpot);
+        boardSquare[i].addEventListener("click", markTheSpot);
       }
     },
-    render: function () {
+    render: function (spot) {
       const boardSquare = document.getElementsByClassName("board-square");
+      const num = spot.getAttribute("data-number");
       const score = this.gameboard;
       for (i = 0; i < score.length; i++) {
-        console.log((boardSquare[i].textContent = score[i]));
+        console.log((boardSquare[num].textContent = score[i]));
       }
     },
   };
 
-  const Player = () => {
-    const playerSpots = [];
+  const Player = (marker) => {
+    const wait = () => {
+      Gameboard.gameboard.push(marker);
+    };
+    return wait;
   };
 
-  Gameboard.addMark();
-  Gameboard.render();
+  const player1 = Player("X");
+  //const player2 = Player("O")
+
+  Gameboard.addMarker();
 })();
+
+// I have to pass in the spot of the click, so that the render puts the array number in the spot
