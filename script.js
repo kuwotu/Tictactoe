b5f4c949b45bfdf9d5e627b50cc9a9f523cd33cd
Kevwe Uwotu <kevwe1996@gmail.com>
2021-08-13 09:37:35 +0100

    making the game flow

--- script.js
+++ script.js
@@ -2,39 +2,58 @@
   const Gameboard = {
     gameboard: [],
 
-    addMarker: function () {
+    // addMarker calls player to add marker into array, gets the div target of the click
+    // and calls the render function with the divs number
+
+    addMarker: function (marker) {
       const boardSquare = document.getElementsByClassName("board-square");
+      markTheSpot = (event) => {
+        const spot = event.target;
+        const spotNumber = spot.getAttribute("data-number");
+        Gameboard.gameboard.push(marker);
+        console.log(this.gameboard);
+        Gameboard.render(spotNumber);
+      };
       for (i = 0; i < boardSquare.length; i++) {
-        markTheSpot = (event) => {
-          const spot = event.target;
-          console.log(this.gameboard);
-          player1();
-          Gameboard.render(spot);
-        };
         boardSquare[i].addEventListener("click", markTheSpot);
       }
     },
-    render: function (spot) {
+
+    // render takes the div clicked (spot) and puts the latest array marker in that div
+    render: function (spotNumber) {
       const boardSquare = document.getElementsByClassName("board-square");
-      const num = spot.getAttribute("data-number");
-      const score = this.gameboard;
+      let score = this.gameboard;
       for (i = 0; i < score.length; i++) {
-        console.log((boardSquare[num].textContent = score[i]));
+        console.log((boardSquare[spotNumber].textContent = score[i]));
       }
+      Gameboard.whosTurn(score);
+    },
+    // trying to add function to control flow of the game
+    whosTurn: function (score) {
+      //checkwhosturn = () => {
+
+      //let score = this.gameboard;
+      if (score[0] === "X" || score[score.length - 1] === "X") {
+        player2.turn();
+      } else player1.turn();
+
+      // };
     },
   };
 
+  // Player is pushing its marker into the Gameboard array
+
   const Player = (marker) => {
-    const wait = () => {
-      Gameboard.gameboard.push(marker);
+    const turn = () => {
+      Gameboard.addMarker(marker);
     };
-    return wait;
+    return { turn };
   };
 
   const player1 = Player("X");
-  //const player2 = Player("O")
+  const player2 = Player("O");
 
-  Gameboard.addMarker();
+  player1.turn();
 })();
 
 // I have to pass in the spot of the click, so that the render puts the array number in the spot
