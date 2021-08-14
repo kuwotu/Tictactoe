6ce8ca465e06c8b257a05b8a5ed4c25a0b553b28
Kevwe Uwotu <kevwe1996@gmail.com>
2021-08-14 10:26:13 +0100

    shaky foundations

--- script.js
+++ script.js
@@ -12,7 +12,7 @@
         const spotNumber = spot.getAttribute("data-number");
         Gameboard.gameboard.push(marker);
         console.log(this.gameboard);
-        Gameboard.render(spotNumber);
+        Gameboard.render(boardSquare, spotNumber);
       };
       for (i = 0; i < boardSquare.length; i++) {
         boardSquare[i].addEventListener("click", markTheSpot);
@@ -20,24 +20,19 @@
     },
 
     // render takes the div clicked (spot) and puts the latest array marker in that div
-    render: function (spotNumber) {
-      const boardSquare = document.getElementsByClassName("board-square");
-      let score = this.gameboard;
+    render: function (boardSquare, spotNumber) {
+      const score = this.gameboard;
       for (i = 0; i < score.length; i++) {
         console.log((boardSquare[spotNumber].textContent = score[i]));
       }
       Gameboard.whosTurn(score);
     },
+
     // trying to add function to control flow of the game
     whosTurn: function (score) {
-      //checkwhosturn = () => {
-
-      //let score = this.gameboard;
       if (score[0] === "X" || score[score.length - 1] === "X") {
         player2.turn();
       } else player1.turn();
-
-      // };
     },
   };
 
@@ -55,5 +50,3 @@
 
   player1.turn();
 })();
-
-// I have to pass in the spot of the click, so that the render puts the array number in the spot
